86682eef4d13ecbb374f5c7cdc79b9901d4100fc
Plyusnina Sofya <114556853+plyussofya@users.noreply.github.com>
2023-04-30 20:38:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const dots = document.querySelectorAll('.dot');
+const events = document.querySelectorAll('.event');
+
+let activeDotIndex = 0;
+let activeDot = dots[activeDotIndex];
+let interval = setInterval(switchDots, 10000);
+
+function switchDots() {
+  activeDot.classList.remove('active-dot');
+  activeDotIndex++;
+  if (activeDotIndex >= dots.length) {
+    activeDotIndex = 0;
+  }
+  activeDot = dots[activeDotIndex];
+  activeDot.classList.add('active-dot');
+  activeDot.dispatchEvent(new MouseEvent('click'));
+}
+
+dots.forEach((dot) => {
+  dot.addEventListener('click', () => {
+	clearInterval(interval);
+    dots.forEach((dot) => dot.classList.remove('active-dot'));
+    activeDot = dot;
+    activeDotIndex = Array.from(dots).indexOf(activeDot);
+    activeDot.classList.add('active-dot');
+    const modal = activeDot.getAttribute('id');
+    events.forEach((event) => {
+      event.classList.remove('active');
+      if (event.getAttribute('id') === modal) {
+        event.classList.add('active');
+      }
+    });
+	interval = setInterval(switchDots, 10000);
+  });
+});
+
+activeDot.dispatchEvent(new MouseEvent('click'));
+
+
+
+
+
+
